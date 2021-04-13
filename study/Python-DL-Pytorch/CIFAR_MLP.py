1e66a493ea67bcc5bf5450cce001d6999c2c71ec
jdj2261 <wlseowhd12@naver.com>
2021-04-13 18:34:50 +0900

    modified CIFAR_MLP.py

--- study/Python-DL-Pytorch/CIFAR_MLP.py
+++ study/Python-DL-Pytorch/CIFAR_MLP.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.optim import optimizer
 from torchvision import transforms, datasets
 
 if torch.cuda.is_available():
@@ -16,10 +17,98 @@ EPOCHS = 10
 
 train_dataset = datasets.CIFAR10(root = "../data/CIFAR_10",
                                   train = True,
-                                  download = True,
+                                  download = False,
                                   transform = transforms.ToTensor())
 
 
-# train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
-#                                             batch_size = BATCH_SIZE,
-#                                             shuffle = True)
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                            batch_size = BATCH_SIZE,
+                                            shuffle = True)
+
+test_dataset = datasets.CIFAR10(root = "../data/CIFAR_10",
+                                train = False,
+                                transform = transforms.ToTensor())
+
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
+                                          batch_size = BATCH_SIZE,
+                                          shuffle = False)
+
+''' 4. 데이터 확인 '''
+for (X_train, y_train) in train_loader:
+    print(f"X_train: {X_train.size()}, type: {X_train.type()}")
+    print(f"y_train: {y_train.size()}, type: {y_train.type()}")
+    break
+
+# pltsize = 1
+# plt.figure(figsize=(10 * pltsize, pltsize))
+
+# for i in range(10):
+#     plt.subplot(1, 10, i + 1)
+#     plt.axis('off')
+#     plt.imshow(np.transpose(X_train[i], (1, 2, 0)))
+#     plt.title('Class: ' + str(y_train[i].item()))
+# plt.show()
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(32 * 32 * 3, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 32*32*3)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim = 1)
+        return x
+
+model = Net().to(DEVICE)
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+criterion = nn.CrossEntropyLoss()
+
+print(model)
+
+def train(model, train_loader, optimizer, log_interval):
+    model.train()
+    for batch_idx, (image, label) in enumerate(train_loader):
+        image = image.to(DEVICE)
+        label = label.to(DEVICE)
+        optimizer.zero_grad()
+        output = model(image)
+        loss = criterion(output, label)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % log_interval == 0:
+            print("Train Epoch: {} [{}/{} ({:.0f}%)]\tTrain Loss: {:.6f}".format(
+                epoch, batch_idx * len(image), 
+                len(train_loader.dataset), 100. * batch_idx / len(train_loader), 
+                loss.item()))
+
+def evaluate(model, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for image, label in test_loader:
+            image = image.to(DEVICE)
+            label = label.to(DEVICE)
+            output = model(image)
+            test_loss += criterion(output, label).item()
+            prediction = output.max(1, keepdim = True)[1]
+            correct += prediction.eq(label.view_as(prediction)).sum().item()
+    
+    test_loss /= (len(test_loader.dataset) / BATCH_SIZE)
+    test_accuracy = 100. * correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+for epoch in range(1, EPOCHS + 1):
+    train(model, train_loader, optimizer, log_interval = 200)
+    test_loss, test_accuracy = evaluate(model, test_loader)
+    print("\n[EPOCH: {}], \tTest Loss: {:.4f}, \tTest Accuracy: {:.2f} % \n".format(
+        epoch, test_loss, test_accuracy))
